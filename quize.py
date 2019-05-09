245d3191b3ee00e6844b85727d344be3f833dd87
carolynemwende <47849072+carolynemwende@users.noreply.github.com>
2019-05-09 18:56:55 +0300

    Add files via upload

---
+++ quize.py
@@ -0,0 +1,38 @@
+def smallest(b):
+	return min(b)
+
+def rem(a):
+	a=(x%3 for x in range(110,150))
+	return a	  
+    
+
+def sorted(a,b,c):
+	k= a+b+c
+	print(k)
+
+
+
+def divisible_by_three(n):
+	div= (range(1,n+1))/3
+	print (div)
+
+
+def flatlist(x):
+	x= [[1,2],[3,4],[5,6]]
+	flatlist=[]
+	for sublist in x:
+		for number in sublist:
+			flatlist.append(number)
+			print(flatlist)
+
+def duplicate():
+	a= set(['a','b','a','e','d','b','c','e','f','g','h'])
+	print(a)
+
+
+# def squares(keys,values):
+# 	a=dict()
+# 	for x in range(149,159)
+
+
+
